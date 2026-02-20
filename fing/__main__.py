1203eac2b0704d6d5a15b35442573fcb83be5e7b
Tom Ritchford <tom@swirly.com>
2026-02-20 15:08:57 +0100

    Add a fingering system checker

--- fing/__main__.py
+++ fing/__main__.py
@@ -1,5 +1,29 @@
+import sys
+from tomlkit import load, exceptions
+
+
+def check(files: list[str]) -> int | None:
+    def check(f: str) -> str | None:
+        try:
+            keys = load(open(f))['key']
+        except exceptions.TOMLKitError as e:
+            return str(e)
+
+        count = {}
+        for k in keys.values():
+            s = k['short_name']
+            count[s] = 1 + count.get(s, 0)
+
+        if dupes := [k for k, v in count.items() if v > 1]:
+            return f'Duplicate keys {dupes}'
+
+    errors = [f'ERROR: {f}: {v}' for f in files if (v := check(f))]
+    print(*(errors or ['ok']), sep='\n', file=sys.stderr)
+    return bool(errors)
+
+
 def main():
-    print("Hello from fing!")
+    sys.exit(check(sys.argv[1:]))
 
 
 if __name__ == "__main__":
