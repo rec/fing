7370cee0361c7d89b2fb10188c628e9ca8fa47c8
Tom Ritchford <tom@swirly.com>
2026-02-24 11:49:50 +0100

    Modify fing/fingering_system.py

--- fing/fingering_system.py
+++ fing/fingering_system.py
@@ -6,26 +6,6 @@ from typing import Any
 
 import tomlkit
 
-NOTE_TO_OFFSET = {
-    'C': 0,
-    'C#': 1,
-    'Db': 1,
-    'D': 2,
-    'D#': 3,
-    'Eb': 3,
-    'E': 4,
-    'F': 5,
-    'F#': 6,
-    'Gb': 6,
-    'G': 7,
-    'G#': 8,
-    'Ab': 8,
-    'A': 9,
-    'A#': 10,
-    'Bb': 10,
-    'B': 11,
-}
-
 
 @dc.dataclass(frozen=True)
 class Key:
@@ -36,9 +16,11 @@ class Key:
 
 class Note:
     def __init__(self, s: str) -> None:
-        self.name = s.replace('_', '').replace('-', '')
-        self.note = self.name[: 1 if self.name[1].isnumeric() else 2]
-        self.octave = int(self.name[len(self.note) :])
+        for k, v in REPLACEMENTS.items():
+            s = s.replace(k, v)
+        self.name = s
+        self.note = s[: 1 if s[1].isnumeric() else 2]
+        self.octave = int(s[len(self.note) :])
         self.note_number = 12 * self.octave + NOTE_TO_OFFSET[self.note]
 
     def __str__(self) -> str:
@@ -147,3 +129,25 @@ def make(filename: str, reraise: bool = True) -> FingeringSystem:
         metadata=spec.metadata,
         to_key=to_key,
     )
+
+
+REPLACEMENTS = {'_': '', '-': '', '♭': 'b', '♯': '#'}
+NOTE_TO_OFFSET = {
+    'C': 0,
+    'C#': 1,
+    'Db': 1,
+    'D': 2,
+    'D#': 3,
+    'Eb': 3,
+    'E': 4,
+    'F': 5,
+    'F#': 6,
+    'Gb': 6,
+    'G': 7,
+    'G#': 8,
+    'Ab': 8,
+    'A': 9,
+    'A#': 10,
+    'Bb': 10,
+    'B': 11,
+}
