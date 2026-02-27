589302277d4fa970fb74beffa42f0ff6664757d4
Tom Ritchford <tom@swirly.com>
2026-02-27 17:39:10 +0100

    Start rendering all

--- fing/render.py
+++ fing/render.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from collections.abc import Sequence
 from xml.etree import ElementTree as ET
 
+from .fingering_system import FingeringSystem
 from .layout import Layout
 
 
@@ -20,3 +21,24 @@ def render(layout: Layout, fingering: Sequence[str], name: str) -> ET.Element:
     attrs = {'x': '40', 'y': str(20 + h - layout.spacing), 'font-size': '60'}
     ET.SubElement(svg, 'text', attrs).text = name
     return svg
+
+
+def render_all(fs: FingeringSystem, layout: Layout) -> None:
+    w, h = layout.size
+    attrs = {'viewBox': f'0 0 {w} {h}', 'xmlns': 'http://www.w3.org/2000/svg'}
+    svg = ET.Element('svg', attrs)
+    style = ET.SubElement(svg, 'style')
+    style.text = layout.style
+
+    ET.SubElement(svg, 'defs').extend(layout.defs)
+    g = ET.SubElement(svg, 'g')
+    note_fingerings = [(n, f) for n, ff in fs.fingerings.items() for f in ff]
+
+    N = len(note_fingerings)
+    rows = 3  # :-)
+    columns = N // rows
+    columns += columns * rows < N
+
+    w, h = layout.size
+
+    assert w and h and g
