a5cfb2fc46048047a8559988a3cd82dda3115e52
Tom Ritchford <tom@swirly.com>
2026-03-14 18:08:24 +0100

    Modify fing/__main__.py

--- fing/__main__.py
+++ fing/__main__.py
@@ -1,11 +1,17 @@
 import sys
 from pathlib import Path
+from typing import Any
+
+from typing_extensions import Never
 
 import tomlkit
 import tyro
 
 
 def tyro_main(config_files: list[Path], /) -> None:
+    def exit(*msgs: Any) -> Never:
+        sys.exit(' '.join(str(i) for i in ('ERROR:', *msgs)))
+
     def load(p: Path) -> tomlkit.TOMLDocument | str:
         try:
             with p.open() as fp:
@@ -13,15 +19,20 @@ def tyro_main(config_files: list[Path], /) -> None:
         except Exception as e:
             return ' '.join(e.args)
 
+    if not config_files:
+        exit('No files')
+
     if missing := [f for f in config_files if not f.exists()]:
-        sys.exit(f'FileNotFound: {" ,".join(str(i) for i in missing)}')
+        exit(f'FileNotFound: {", ".join(str(i) for i in missing)}')
 
     loaded = {p: load(p) for p in config_files}
+    if len(loaded) != len(config_files):
+        print('WARNING: duplicate input files', file=sys.stderr)
     if errors := {k: v for k, v in loaded.items() if isinstance(v, str)}:
         msgs = '\n'.join(f'{k}: {v}' for k, v in errors.items())
         e = len(errors) != 1
         s, n = 's' * e, '\n' * e
-        sys.exit(f'TOML error{s}: {n}{msgs}')
+        exit(f'TOML error{s}: {n}{msgs}')
 
     bases = [k for k, v in loaded.items() if list(v) != ['layout']]
     layouts = [k for k, v in loaded.items() if list(v) == ['layout']]
@@ -29,11 +40,11 @@ def tyro_main(config_files: list[Path], /) -> None:
 
     fnames = ' ,'.join(str(i) for i in loaded)
     if len(bases) != 1:
-        sys.exit(f'{len(bases)} fingering files found in {fnames}')
+        exit(f'{len(bases)} fingering files found')
     if layouts and not non_styles:
-        sys.exit(f'Styles without layouts found in {fnames}')
+        exit('Styles without layouts found')
     if layouts and len(non_styles) > 1:
-        sys.exit(f'Too many layouts found in {fnames}')
+        exit('Too many layouts found')
 
     (base,) = bases
 
