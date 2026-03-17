aef53da489a579dd94f19038ad77e2c2ee62d4f4
Tom Ritchford <tom@swirly.com>
2026-03-17 17:00:16 +0100

    Add switch for experimentation to __main__.py

--- fing/__main__.py
+++ fing/__main__.py
@@ -1,12 +1,18 @@
 import sys
+from pathlib import Path
 
 import tyro
 
 from .render_chart import Exit, render_chart
 
+USE_TYRO = True
+
 
 def main():
     try:
-        tyro.cli(render_chart)
+        if USE_TYRO:
+            tyro.cli(render_chart)
+        else:
+            render_chart([Path(i) for i in sys.argv[1:]])
     except Exit as e:
         print('ERROR:', *e.args, file=sys.stderr)
